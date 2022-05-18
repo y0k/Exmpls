5bc154490fc09280f9fab0d58127021c0183f75b
Kirill <cyrillepishin@gmail.com>
2022-05-18 15:21:25 +0300

    передать между активностями произвольный текст

--- app/src/main/java/com/example/exmpls/MainActivity2.java
+++ app/src/main/java/com/example/exmpls/MainActivity2.java
@@ -1,25 +1,51 @@
 package com.example.exmpls;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+
+import android.view.View;
+import android.widget.Button;
+
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
 
-    TextView textView;
-    EditText editText;
+public class MainActivity2 extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private Button back;
+    private TextView name;
+    private TextView lastName;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
 
+        back = (Button) findViewById(R.id.back_button);
+        name = (TextView) findViewById(R.id.nametxt);
+        lastName = (TextView) findViewById(R.id.lastnametxt);
 
+        // Принимаем имя
+        String txtName = getIntent().getStringExtra("name");
 
+        // Принимаем фамилию
+        String txtLastname = getIntent().getStringExtra("lastname");
 
+         // выводим принятое имя
+        name.setText(name.getText().toString() + " " + txtName);
+
+         // Выводим принятую фамилию
+        lastName.setText(lastName.getText().toString() + " " + txtLastname);
+    }
+
+    public void back(View v){
+        switch (v.getId()) {
+            case R.id.back_button:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
