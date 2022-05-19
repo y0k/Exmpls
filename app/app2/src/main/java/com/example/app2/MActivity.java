c9b8c804942502c1c6967018e0ffc9ade2e46156
Kirill <cyrillepishin@gmail.com>
2022-05-19 13:54:06 +0300

    after click block butt

--- app/app2/src/main/java/com/example/app2/MActivity.java
+++ app/app2/src/main/java/com/example/app2/MActivity.java
@@ -2,10 +2,8 @@ package com.example.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,35 +20,38 @@ public class MActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mactivity);
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "onClick!!!", Toast.LENGTH_SHORT).show();
+
 
         button = findViewById(R.id.btn);
         enter = findViewById(R.id.textView);
 
 
+    }
+
+       /*button.setOnClickListener(new View.OnClickListener() {*/
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
 
 
                 switch (v.getId()) {
                     case R.id.btn:
-
+                        Toast.makeText(this, "НАЖАЛ!!!", Toast.LENGTH_SHORT).show();
                         EditText editText = findViewById(R.id.enter);
                         TextView textView = findViewById(R.id.textView);
                         String txtName = editText.getText().toString();
                         textView.setText(txtName);
+                        v.setClickable(false);
 
                         break;
                     default:
                         break;
+
                 }
             }
-        });
+        /*});*/
 
-        }
+
 
 
     }
