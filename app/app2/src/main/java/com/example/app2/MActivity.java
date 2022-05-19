8c57ddf99a02a0818ed49d12a1ca2a8740f8c8fb
Kirill <cyrillepishin@gmail.com>
2022-05-19 13:41:44 +0300

    editT to TView by butt click

---
+++ app/app2/src/main/java/com/example/app2/MActivity.java
@@ -0,0 +1,57 @@
+package com.example.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView enter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mactivity);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "onClick!!!", Toast.LENGTH_SHORT).show();
+
+        button = findViewById(R.id.btn);
+        enter = findViewById(R.id.textView);
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                switch (v.getId()) {
+                    case R.id.btn:
+
+                        EditText editText = findViewById(R.id.enter);
+                        TextView textView = findViewById(R.id.textView);
+                        String txtName = editText.getText().toString();
+                        textView.setText(txtName);
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        }
+
+
+    }
+
