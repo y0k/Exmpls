5bbc41b0ce2b1d84db8f8e61cda6effeab3f775d
Kirill <cyrillepishin@gmail.com>
2022-05-22 20:24:16 +0300

    release

--- app/fproj/src/main/java/com/example/fproj/DBHelper.java
+++ app/fproj/src/main/java/com/example/fproj/DBHelper.java
@@ -8,15 +8,20 @@ import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(@Nullable Context context) {
-        super(context, "myDb", null, 1);
+        super(context, "myDb.db", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL("create table mytable ("+ "id integer primary key autoincrement,"
-        +"name text, "+"text"+");");
+        +"text text"+");");
 
+    }
+
+    @Override
+    public SQLiteDatabase getReadableDatabase() {
+        return super.getReadableDatabase();
     }
 
     @Override
--- app/fproj/src/main/java/com/example/fproj/MainActivity.java
+++ app/fproj/src/main/java/com/example/fproj/MainActivity.java
@@ -6,19 +6,22 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     final String LOG_TAG = "myLogs";
     Button btnAdd,btnRead,btnClear;
-    EditText /*etName,*/etText;
-
+    EditText etText;
+    TextView name;
     DBHelper dbHelper;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,27 +33,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnClear = findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
 
-        /*etName = findViewById(R.id.etName);*/
         etText = findViewById(R.id.etText);
         //объект для создания и управоения версиями бд
         dbHelper = new DBHelper(this);
-    }
 
+        name = findViewById(R.id.nametxt);
+    }
     @Override
     public void onClick(View v) {
         //object dlya dannyx
         ContentValues cv = new ContentValues();
         //poluchaem dannie iz polei vvoda
-       /* String name = etName.getText().toString();*/
         String text = etText.getText().toString();
         //connect to Db
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-
         switch (v.getId()) {
             case R.id.btnAdd:
                 Log.d(LOG_TAG, "INSERT");
                 //podgotovim dannie dlya vstavki d vide par: stolbec-znachenie
-               /* cv.put("name", name);*/
                 cv.put("text", text);
                 //vstavlyaem zapis i poluchaem ee id
                 long rowId = db.insert("mytable",null,cv);
@@ -61,20 +61,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //zapros vcex dannyx iz table i poluchaem Cursor
                 Cursor c = db.query("mytable", null,null,null,null,null,null);
                 //stavim Cursor na pervoe mesto, esli strok net - false
+
+                List itemIds = new ArrayList<>();
+
                 if (c.moveToFirst()) {
                     //opredelyaem imena stolbcov po imeni v vyborke
                     int idColIndex = c.getColumnIndex("id");
-                    /*int nameColIndex = c.getColumnIndex("name");*/
                     int textColIndex = c.getColumnIndex("text");
                     do {
                         Log.d(LOG_TAG,"id= " + c.getInt(idColIndex)+
-                                /*",  name = " + c.getString(nameColIndex)+*/
                                 ", text= " + c.getString(textColIndex));
+                        int itemId = c.getInt(idColIndex);
+                        itemIds.add(itemId);
+                        itemIds.add(c.getString(textColIndex));
+
                         // perexod na sled stroky, esli net sled to false- vixod iz cikla
                     } while(c.moveToNext());
                 } else
                     Log.d(LOG_TAG,"0 rows");
                 c.close();
+                name.setText(itemIds.toString().replace(", ","\n" ).replace("]", "").replace("[", ""));
                 break;
             case R.id.btnClear:
                 Log.d(LOG_TAG,"CLEAR ALL");
@@ -83,7 +89,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
         dbHelper.close();
-
-
     }
 }
